8446733e41e335d556e55a300a6e522e15b7d0f6
MariahRojas <51241518+MariahRojas@users.noreply.github.com>
2019-06-17 11:24:15 -0400

    Updated code.
    I changed my listItems variable. I realized i can access the student list by getting the class for li. I switched around a few variables and added my if statement. Called showPage... aaaand nothing.

--- js/script.js
+++ js/script.js
@@ -17,9 +17,10 @@ FSJS project 2 - List Filter and Pagination
    scoped to that function.
 ***/
 
+const listItems = document.querySelectorAll(".student-item"); // Create a variable to store the student list item elements in the student list. 
+let itemsPerPage = 10; // Create a variable to store the number of items to show on each “page”, which for this project, is 10.
 
-
-
+console.log(listItems);
 /*** 
    Create the `showPage` function to hide all of the items in the 
    list except for the ten you want to show.
@@ -35,6 +36,19 @@ FSJS project 2 - List Filter and Pagination
        "invoke" the function 
 ***/
 
+const showPage = (list, page) => {
+   let startIndex = (page * itemsPerPage) - itemsPerPage;
+   let endIndex = page * itemsPerPage;
+   for (let index = 0; index < list.length; index++) {
+      if (index <= startIndex && index >= endIndex) {
+         list[index].style.dislay = "none";
+      } else {
+         list[index].style.display = "block";
+      }
+   }
+}
+
+showPage(listItems, 1);
 
 
 
